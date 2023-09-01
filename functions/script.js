90e68bc9d006cd1fb9fb134b1877d6afbdba576f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-01 20:40:55 +0300

    set default parameters

--- functions/script.js
+++ functions/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 2,
+  price = 199 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// numPassengers olarak 2 yi alacak, default olarak 2 ayarlamıştım.
+createBooking('LH123', undefined, 1000);
