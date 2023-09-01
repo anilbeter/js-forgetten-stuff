bab2b3a0ad7b229ea5d48d3e2b39673e08ce9aa0
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-01 20:54:25 +0300

    how passing arguments works: value vs. reference

--- functions/script.js
+++ functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -23,3 +24,34 @@ createBooking('LH123', 5);
 
 // numPassengers olarak 2 yi alacak, default olarak 2 ayarlamıştım.
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const anil = {
+  name: 'Anil Beter',
+  passport: 2342343,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2342343) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, anil);
+console.log(flight);
+// LH234
+console.log(anil);
+// {name: 'Mr. Anil Beter', passport: 2342343}
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+
+newPassport(anil);
+checkIn(flight, anil);
