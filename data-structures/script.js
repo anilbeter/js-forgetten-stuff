9ef2ec44ae3faf458def989a1a6b5dd7d4a943b7
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-04 15:15:00 +0300

    add challenge #3

--- data-structures/script.js
+++ data-structures/script.js
@@ -53,6 +53,56 @@ const restaurant = {
     console.log(optionalIngredients);
   },
 };
+
+// Challenge #3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+// (4) ['⚽ GOAL', '🔁 Substitution', '🔶 Yellow card', '🔴 Red card']
+
+// 2)
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3)
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4)
+for (const [key, value] of gameEvents) {
+  console.log(
+    `${key < 45 ? '[FIRST HALF]' : '[SECOND HALF] '} ${key}: ${value}`
+  );
+}
+
+/*
+Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽GOAL
+GOOD LUCK 😀
+
+/////////////////////////////////
 // Maps: Iteration
 
 const question = new Map([
@@ -87,7 +137,7 @@ for (const [key, value] of question) {
 console.log([...question]);
 // (8) [Array(2), Array(2), Array(2), Array(2), Array(2), Array(2), Array(2), Array(2)]
 
-/*
+
 // Maps Fundamentals
 
 // Long story short -> Maps just like objects, but indeed there's one huge difference between them. You can set keys only string in OBJECTS, againts this you can set any type keys in MAPS. Like number, arrays, even you can use a map for a key!
