abdb3045b99454f150c6039f0acdfddd37115096
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-09 20:49:39 +0300

    Functions Accepting Callback Functions

--- functions/script.js
+++ functions/script.js
@@ -56,3 +56,37 @@ const newPassport = function (person) {
 newPassport(anil);
 checkIn(flight, anil);
 */
+
+// Functions Accepting Callback Functions (High-Order Function)
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+// Original string: JavaScript is the best!
+// Transformed string: JAVASCRIPT is the best!
+// Transformed by: upperFirstWord
+
+transformer('My dreams come through', oneWord);
+// Original string: My dreams come through
+// Transformed string: mydreamscomethrough
+// Transformed by: oneWord
+
+// addEventListener also higher-order function
+const high5 = function () {
+  console.log(`👋🏻`);
+};
+document.body.addEventListener('click', high5);
