3b36d897a2d466dc13c0da98042d2c465a8aa993
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-09-13 13:55:32 +0300

    add my own solution for challenge #1

--- advanced-arrays/script.js
+++ advanced-arrays/script.js
@@ -224,3 +224,19 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
 */
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+
+const checkDogs = function (julia, kate) {
+  const shallowCopyJulia = julia.slice();
+  const splicedJulia = shallowCopyJulia.splice(1, 2);
+  const totalData = splicedJulia.concat(kate);
+  totalData.forEach((age, i) => {
+    if (age >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${age} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still puppy (${age})`);
+    }
+  });
+};
+checkDogs(dogsJulia, dogsKate);
