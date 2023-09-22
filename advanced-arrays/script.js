203ac964f121fa54bb7fa0054a56d20a495d523f
Anil Beter <86671685+anilbeter@users.noreply.github.com.>
2023-09-22 15:20:07 +0300

    introduction to method chaining

--- advanced-arrays/script.js
+++ advanced-arrays/script.js
@@ -357,7 +357,7 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
-*/
+
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? age * age : 16 + age * 4));
   const adult = humanAges.filter(age => age >= 18);
@@ -370,3 +370,12 @@ const calcAverageHumanAge = function (ages) {
 };
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+*/
+
+// The Magic Of Chaining Methods
+const eurToUsd = 1.1;
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
