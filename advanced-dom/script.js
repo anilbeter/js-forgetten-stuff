81fad0950456b1b31aa432a6a801dcfd6a08baac
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-09 21:00:34 +0300

    rid of temp slider styles

--- advanced-dom/script.js
+++ advanced-dom/script.js
@@ -277,9 +277,9 @@ let currentSlide = 0;
 const maxSlide = slides.length;
 
 // TEMP (just for working)
-const slider = document.querySelector('.slider');
-slider.style.transform = 'scale(0.4) translateX(-800px)';
-slider.style.overflow = 'visible';
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
 
 const goToSlide = function (slide) {
   slides.forEach(
