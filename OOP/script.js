2d6932ce968684ed71eb171112db6c861ee4e52c
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-11 10:13:15 +0300

    consturctor function

--- OOP/script.js
+++ OOP/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+// Constructor Function
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // BAD PRACTICE - never create method inside of constructor function
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
+};
+
+const anil = new Person('Anil', 1984);
+console.log(anil);
+// Person {firstName: 'Anil', birthYear: 1984}
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const billie = new Person('Billie', 2023);
+const steve = new Person('Steve', 1999);
+
+// Check is instance or not
+console.log(anil instanceof Person);
+// true
+console.log(billie instanceof Person);
+// true
+const micheal = 'micheal';
+console.log(micheal instanceof Person);
+// false
